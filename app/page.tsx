9eec6dd745c3fa61e98fa8314973f5c314d7f6bf
Victor Arabi <victorarabi@hotmail.com>
2025-03-14 15:29:12 -0400

    feature - created base files, added CBC font and base app structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
-import SVGIMG from "./cbcLogo.svg";
+import SVGIMG from "@/public/cbcLogo.svg";
+import electionResults from "@/app/services.ts/electionData";
 
-export default function Home() {
+export default async function Home() {
+  const data = await electionResults();
+  const { parties, ridings, extras } = data;
   return (
     <div className="flex flex-col p-4 gap-8">
       <Image width={40} src={SVGIMG} alt={""} />
